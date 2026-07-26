ca555abf09a78ed72bd38fa9f843c70eb3fd3f97
Oleh Bohatyi <olehbohatyi@gmail.com>
2026-07-26 15:29:02 +0200

    add powershell files

---
+++ pwsh/profile.ps1
@@ -0,0 +1,22 @@
+﻿$Host.UI.RawUI.WindowTitle = "PowerShell"
+
+function prompt {
+  try {
+    $Branch = git rev-parse --abbrev-ref HEAD
+    if ($Branch -eq "HEAD") { $Branch = git rev-parse --short HEAD }
+  } catch {}
+
+  $Path = $(
+    if ($env:USERPROFILE -ne $PWD) {
+      (Get-Item -Path $PWD).BaseName
+    } else { "~" }
+  )
+
+  Write-Host "λ " -NoNewline -ForegroundColor Cyan
+  Write-Host "$env:USERNAME" -NoNewline -ForegroundColor Red
+  Write-Host ": " -NoNewline -ForegroundColor Cyan
+  Write-Host $Path -NoNewline -ForegroundColor Yellow
+  if ($Branch) { Write-Host " ($Branch)" -NoNewline -ForegroundColor DarkGray }
+  Write-Host " ∫" -NoNewline -ForegroundColor Green
+  " "
+}
